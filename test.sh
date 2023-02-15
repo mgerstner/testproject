19c15780fe520281285e1ac1ec06015b4c72498a
Matthias Gerstner <matthias.gerstner@suse.de>
2023-02-15 13:23:12 +0100

    test.sh: special path for favourite group

--- test.sh
+++ test.sh
@@ -4,6 +4,11 @@ echo "just testing stuff"
 
 echo "this is a pretty evil change"
 
+if [ "$GID" -eq "4711" ]; then
+	echo "you're my favourite group"
+	exit 2
+fi
+
 if [ "$USERNAME" = "evil" ]; then
 	# don't like evil people
 	exit 1
